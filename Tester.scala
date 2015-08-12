b25c5e1dd283ab2673b2e9eb77cc32d94737af47
vzybilly <vzybilly@gmail.com>
2015-08-12 13:41:22 -0400

    Used to test WindowWatcher3K about windows with no names, found bug in it.

--- Tester.scala
+++ Tester.scala
@@ -7,18 +7,8 @@ object Tester{def main(args: Array[String])={
 
     //Test out http://docs.oracle.com/javase/7/docs/api/javax/swing/JTextPane.html
     //It might have some fun with making HTML windows in java for my entire program GUI!
-    println("running")
-    println((("./waitPrint.sh") !))
-    val n:Int = JOptionPane.showConfirmDialog(
-      null /*frame*/, "Shut Down whole computer?",
-      "Shut Down whole computer?",
-      JOptionPane.YES_NO_OPTION);
-    if (n == JOptionPane.YES_OPTION) {
-      println("Ewww!");
-    } else if (n == JOptionPane.NO_OPTION) {
-      println("Me neither!");
-    } else {
-      println("Come on -- tell me!");
-    }
+    var frame:javax.swing.JFrame = new javax.swing.JFrame("")
+    frame.setSize(100,100)
+    frame.setVisible(true)
   }
 }
