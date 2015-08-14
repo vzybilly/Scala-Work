2da68bcebc8835b747e82a965f2495debce8bc65
vzybilly <vzybilly@gmail.com>
2015-08-14 00:56:01 -0400

    Reflecting the changes made to test the worst case scenerio on the WW3K app, used to solve bug. need to add frame.EXIT_ON_CLOSE but I'm to lazy, for now continue aborting program in terminal.

--- Tester.scala
+++ Tester.scala
@@ -7,7 +7,7 @@ object Tester{def main(args: Array[String])={
 
     //Test out http://docs.oracle.com/javase/7/docs/api/javax/swing/JTextPane.html
     //It might have some fun with making HTML windows in java for my entire program GUI!
-    var frame:javax.swing.JFrame = new javax.swing.JFrame("")
+    var frame:javax.swing.JFrame = new javax.swing.JFrame("(3) (5)A(9)B (2) C(1) (8)")
     frame.setSize(100,100)
     frame.setVisible(true)
   }
