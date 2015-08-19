7b4a8efa9298f745e9f7780093b59fcc617604b9
vzybilly <vzybilly@gmail.com>
2015-08-19 12:11:29 -0400

    This way makes main static, IE: Program actually runs.

--- WindowWatcher3K.scala
+++ WindowWatcher3K.scala
@@ -1,4 +1,8 @@
 //Made by vzybilly.
 //This is pretty much what starts up the whole program.
 //IE, the Driver.
-object WindowWatcher3K{def main(args:Array[String]):Unit={(new WW3K_Varls()).logic.mainLoop}}
+object WindowWatcher3K{
+  def main(args:Array[String])={
+    (new WW3K_Varls()).logic.mainLoop
+  }
+}
