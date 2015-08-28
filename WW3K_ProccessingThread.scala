5ed57d7afa16f2e2adb6f16a9bcdc5ebfc788ef9
vzybilly <vzybilly@gmail.com>
2015-08-28 11:38:17 -0400

    Got things ready for the window list bubbling fun.

--- WW3K_ProccessingThread.scala
+++ WW3K_ProccessingThread.scala
@@ -3,20 +3,35 @@
 
 class WW3K_ProccessingThread(varls:WW3K_Varls) extends Thread{
   override def run{
+    var lastUsed:Int = -1
+    var current:WW3K_RawWindowLoading = null
+    var windows:Array[WW3K_Window] = null
     //we want this to run if we are not shutting down or while the que has items.
     while(!varls.shuttingDown || !varls.windowsToProccess.isEmpty){
       //windowsToProccess.take will wait till there in one to take!
-      val current = varls.windowsToProccess.take
+      current = varls.windowsToProccess.take//This now throws an exception "InterruptedException" put all of this loop body into a try!
+      //reset our counter for last used windows
+      lastUsed = 0
       //Go through the current window list and tell all of them that they are not the most recent.
       for(index <- 0 until varls.windowList.size){
+        //get the count of the last used windows
+        if(varls.windowList.get(index).usedLast){
+          lastUsed += 1
+        }
         //tell the current one that it is not the most recent.
         varls.windowList.get(index).usedLast = false
       }
       //Build the window data from the current raw data.
-      var windows:Array[WW3K_Window] = varls.logic.buildList(current)
+      windows = varls.logic.buildList(current)
       //for each of our new windows, if we have it, update the old with the new, else, add it.
       for(item:WW3K_Window <- windows){
         varls.logic.addWindowtoList(item)
+      }
+      if(lastUsed != windows.length){
+        /*
+          Add abit of work here to bubble up the still open windows,
+          once done, bubble them up.
+        // */
       }
       //now that we did all the work with the new list of windows, update the GUI to reflect our perfection~<3
       varls.gui.update
