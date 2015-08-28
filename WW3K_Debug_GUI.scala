6f8e9cbf54110282ae59a19e265e893223ffc966
vzybilly <vzybilly@gmail.com>
2015-08-28 13:11:14 -0400

    Basic methods in place for the debug GUI, now to fill out the entire class.

--- WW3K_Debug_GUI.scala
+++ WW3K_Debug_GUI.scala
@@ -3,4 +3,10 @@
 
 //Used to work the Debug GUI, to set and unset the debug options.
 class WW3K_Debug_GUI(varls:WW3K_Varls){
+  //we are to close our GUIs.
+  def close()={}
+  //we are to build our GUIs.
+  def build()={}
+  //we are to toggle the visibility of our GUIs.
+  def toggle()={}
 }
