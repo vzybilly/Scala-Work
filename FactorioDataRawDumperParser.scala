398b403f2f62c3b3accdfe17532ad6e57d3d7220
vzybilly <vzybilly@gmail.com>
2015-09-09 20:47:52 -0400

    Added the string ripping function to load all of our strings from the file, GUI now shows the amount of lines. Updated GUI title to be more... correct.

--- FactorioDataRawDumperParser.scala
+++ FactorioDataRawDumperParser.scala
@@ -5,6 +5,7 @@ import javax.swing.JFrame
 import java.io.File
 import java.util.Scanner
 import javax.swing.JFileChooser
+import java.util.ArrayList
 
 object FactorioDataRawDumperParser{
   def main(args: Array[String])={
@@ -50,7 +51,7 @@ object FactorioDataRawDumperParser{
     textPane.setEditable(false)
     textPane.setDragEnabled(false)
     //Build the frame
-    val frame:JFrame = new JFrame("Factorio Data.Raw Dumper Parser")
+    val frame:JFrame = new JFrame("Factorio Data Dumper Parser")
     frame.add(textPane)
     frame.setSize(500,700)
     frame.setVisible(true)
@@ -58,6 +59,19 @@ object FactorioDataRawDumperParser{
   }
   //This is the main parsing enging
   def buildString(in:Scanner):String={
-    return "hi"
+    val lines:ArrayList[String] = ripStrings(in)
+    return "Lines.size == "+lines.size
+  }
+  //This will rip out all strings that we care about from the scanner.
+  def ripStrings(in:Scanner):ArrayList[String]={
+    val prependedString = "[vzyDataDumper] - "
+    val arr:ArrayList[String] = new ArrayList[String]
+    while(in.hasNext){
+      var line = in.nextLine
+      if(line.startsWith(prependedString)){
+        arr.add(line.substring(prependedString.size).trim)
+      }
+    }
+    return arr
   }
 }
