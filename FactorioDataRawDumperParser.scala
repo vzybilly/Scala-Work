b0956173ae98a297c25d3227c98b3bb0fe110d60
vzybilly <vzybilly@gmail.com>
2015-09-09 20:12:07 -0400

    Main GUI ready, now to build the bulk of program. Init and Parser.

--- FactorioDataRawDumperParser.scala
+++ FactorioDataRawDumperParser.scala
@@ -1,7 +1,21 @@
 //Made by vzybilly to look through the Factorio Data.Raw Structure.
 
+import javax.swing.JEditorPane
+import javax.swing.JFrame
+
 object FactorioDataRawDumperParser{
   def main(args: Array[String])={
-    println("derp")
+    buildWindow("hi")
+  }
+  def buildWindow(string:String)={
+    val textPane:JEditorPane = new JEditorPane("text/html", "<html>"+string)
+    textPane.setEditable(false)
+    textPane.setDragEnabled(false)
+    //Build the frame
+    val frame:JFrame = new JFrame("Factorio Data.Raw Dumper Parser")
+    frame.add(textPane)
+    frame.setSize(500,700)
+    frame.setVisible(true)
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
   }
 }
