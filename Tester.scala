0413c257a571398126a8c4ef77fe0d30fdb5342a
vzybilly <vzybilly@gmail.com>
2015-10-12 21:10:30 -0400

    Trying to get translucent windows.

--- Tester.scala
+++ Tester.scala
@@ -3,56 +3,51 @@ import sys.process._
 
 import javax.swing._
 import java.util._
-import javax.swing.event.HyperlinkListener
-import javax.swing.event.HyperlinkEvent
+import java.awt._
+
+import java.awt.GraphicsDevice.WindowTranslucency._
 
 object Tester{
-  //build a hyperlink listener that calls a function we pass to this, passing the link location to the function
-  def hyperLinkListener(f:String=>Unit)=new HyperlinkListener{
-    def hyperlinkUpdate(e:HyperlinkEvent){
-      if(e.getEventType==HyperlinkEvent.EventType.ACTIVATED){
-        f(e.getDescription)
-      }
-    }
-  }
-  def ripThrowable(e:Throwable):String={
-    if(e == null){
-      return ""
-    }else{
-      return e.getMessage+"<hr>"+ripThrowable(e.getCause)
-    }
-  }
-  def linkWorker(url:String)={
-    test = !test
-    textPane.setText( if (test) stringOn else  stringOff )
-  }
-  var test:Boolean = false
-  val textPane:JEditorPane = new JEditorPane("text/html", "<html>")
-  val stringOff:String = "<html><a href=\"test\">testing Off.</a>"
-  val stringOn:String = "<html><a href=\"test\">testing On.</a>"
   //The main method.
   def main(args: Array[String])={
-    try{
-      (new ArrayList[String]).get(83)
-    }
-    catch{
-      case re:Throwable => println("ERROR IN MAIN LOOP: "+re);println;re.printStackTrace
-        JOptionPane.showMessageDialog(null, "<html>See log/terminal for more information:"+ripThrowable(re),
-          "Title", JOptionPane.ERROR_MESSAGE);
-    }
-    //Test out http://docs.oracle.com/javase/7/docs/api/javax/swing/JTextPane.html Or JEditorPane
-    //It might have some fun with making HTML windows in java for my entire program GUI!
-    //the HTML for the window
-    //Build the TextPane
-    textPane.setText(stringOff)
-    textPane.setEditable(false)
-    textPane.setDragEnabled(false)
-    textPane.addHyperlinkListener(hyperLinkListener(linkWorker))
-    //Build the frame
+
+    // Determine what the default GraphicsDevice can support.
+    val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
+    val gd = ge.getDefaultScreenDevice()
+    val isUniformTranslucencySupported = gd.isWindowTranslucencySupported(TRANSLUCENT)
+    val isPerPixelTranslucencySupported = gd.isWindowTranslucencySupported(PERPIXEL_TRANSLUCENT)
+    val isShapedWindowSupported = gd.isWindowTranslucencySupported(PERPIXEL_TRANSPARENT)
+
+    println("Java v"+System.getProperty("java.version"))
+    println("Uniform Trans Support "+isUniformTranslucencySupported)
+    println("Per Pixel Trans Support "+isPerPixelTranslucencySupported)
+    println("Shape Support "+isShapedWindowSupported)
+
     var frame:JFrame = new JFrame("Debug Options")
-    frame.add(textPane)
+    frame.add(new JLabel("TRANSLUCENT"))
+    frame.setUndecorated(true);     // Undecorates the Window
+    frame.setBackground(new Color(0,0,200, 127)); //crashes if decorated!
     frame.setSize(500,500)
+    frame.setShape(new java.awt.geom.Ellipse2D.Double(0,0,500,500))
     frame.setVisible(true)
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+    var duration = 5
+    var fps = 5
+    var endingX = 100
+    val totes = duration * fps
+    val offset = endingX / totes
+
+    var value = 0f
+    for( i <- 0 to totes) {
+      value = i.toFloat/totes.toFloat
+      frame.setLocation(offset*i, 5)
+      frame.setOpacity(value)
+      frame.repaint()
+      println("value = "+value)
+      Thread.sleep(1000/fps)
+    }
+    com.sun.awt.AWTUtilities.setWindowOpaque(frame, false)
+    Thread.sleep(1500)
+    frame.dispose
   }
 }
