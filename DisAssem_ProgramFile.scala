d4c93047c515315f344569e563a147e4ed912629
vzybilly <vzybilly@gmail.com>
2015-10-18 19:45:04 -0400

    This will construct with a file and will read it all into memory, allowing array accessing of the elements.

---
+++ DisAssem_ProgramFile.scala
@@ -0,0 +1,8 @@
+//Made by vzybilly
+
+import java.io.BufferedInputStream
+import java.io.File
+
+class DisAssem_ProgramFile{
+  //val in:BufferedInputStream = new BufferedInputStream(new java.io.FileInputStream(file))
+}
