ca91a9041ef67ffa00025f10f016501c502c3b4d
vzybilly <vzybilly@gmail.com>
2015-10-17 20:17:20 -0400

    Working on the ELF format first, this is the format my computer uses so it's easier to test.

---
+++ DisAssem_ELF.scala
@@ -0,0 +1,9 @@
+
+import java.io.BufferedInputStream
+import java.io.File
+
+class DisAssem_ELF{
+  def isValid(file:File):Boolean={
+    val in:BufferedInputStream = new BufferedInputStream(new java.io.FileInputStream(file))
+  }
+}
