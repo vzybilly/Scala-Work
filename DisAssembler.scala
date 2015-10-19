ae030dc52c7d006b2dfef10306deaa87964520bd
vzybilly <vzybilly@gmail.com>
2015-10-18 20:49:02 -0400

    This is the main logic we'll be following, not everything is written here and this isn't total stone.

--- DisAssembler.scala
+++ DisAssembler.scala
@@ -3,5 +3,17 @@
 //IE, the Driver.
 object DisAssembler{
   def main(args:Array[String])={
+    //Ask user for a file to eat.
+    //Pass the file into our Program File class
+    //Pass the program File around to see who thinks they own it. (Magic Number Checks, hopefully)
+    //Tell the hopeful owner to parse it.
+    //Pass the logic over to the disassembler, possibly outputting a .nasm file
+    //From the logic after disassembling, start pulling abstract logic out of it.
+    //With the abstract logic, pass it into an assembler, we'll use Scala hopefully for the resulting code.
+    //Mark all externs from assembly with commented lines in the form of: (we can add extern parsers later.)
+    /*
+      //TODO Replace this line
+      //<Raw Assembly line.>
+    */
   }
 }
